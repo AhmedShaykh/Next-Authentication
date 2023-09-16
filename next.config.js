1413c1ed5de93884b3e74f06bf713a169c2841c1
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-09-16 21:50:07 +0500

    Delete

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    env: {
-        DATABASE_URL: "mongodb+srv://admin:admin1234@cluster0.jivsgsq.mongodb.net/nextAuth"
-    }
-};
-
-module.exports = nextConfig;
