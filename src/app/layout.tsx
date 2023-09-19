be7fd37bd0c1e433ed0ecf09ac4999e75702fbf1
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-09-19 22:06:47 +0500

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Next Authentication",
+  description: "Learning Next Authentication"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-zinc-950 text-slate-400`}>
+        {children}
+      </body>
+    </html>
+  )
+};
